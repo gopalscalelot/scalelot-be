6e8b9ac6383604e9fcf83888d64eef43999fcfe0
kishan7622 <gopal.scalelot@gmail.com>
2022-05-03 17:54:16 +0530

    heroku

--- build/config/Config.js
+++ build/config/Config.js
@@ -5,20 +5,20 @@ exports.logDirectory = exports.corsUrl = exports.GMAIL_CONFIG = exports.PROTECTE
 exports.environment = process.env.NODE_ENV;
 exports.port = process.env.PORT;
 exports.db = {
-    connectionString: exports.environment == 'production' ? process.env.PROD_DB_CONNECTION_STRING : process.env.DB_CONNECTION_STRING || '',
+    connectionString: exports.environment == "mongodb+srv://gopal_scalelot:root%40Scalelot@cluster0.xjusu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     user: exports.environment == 'production' ? process.env.PROD_DB_USER : process.env.DB_USER || '',
     password: exports.environment == 'production' ? process.env.PROD_DB_PASSWORD : process.env.DB_PASSWORD || '',
 };
 exports.DEFAULT_USER_CREDS = {
-    userName: process.env.DEFAULT_USERNAME || '',
-    password: process.env.DEFAULT_PASSWORD || ''
+    userName: process.env.DEFAULT_USERNAME || 'admin',
+    password: process.env.DEFAULT_PASSWORD || 'admin'
 };
-exports.PROTECTED_ENDPOINTS = process.env.PROTECTED_ENDPOINTS.split(",");
+exports.PROTECTED_ENDPOINTS = "/v1/meta-keywords,/v1/contact-us/all,/v1/career/all".split(",");
 exports.GMAIL_CONFIG = {
-    service: process.env.GMAIL_SERVICE || '',
-    user: process.env.GMAIL_AUTH_USERNAME || '',
-    password: process.env.GMAIL_AUTH_PASSWORD || '',
+    service: process.env.GMAIL_SERVICE || "gmail",
+    user: process.env.GMAIL_AUTH_USERNAME || "sales.scalelot@gmail.com",
+    password: process.env.GMAIL_AUTH_PASSWORD || "SalesScalelot@22",
 };
-exports.corsUrl = process.env.CORS_URL;
-exports.logDirectory = process.env.LOG_DIR;
+exports.corsUrl = process.env.CORS_URL || "*";
+exports.logDirectory = process.env.LOG_DIR || "/";
 //# sourceMappingURL=Config.js.map
